c17c350eec73173b41a1089ca869074e68df024f
Unknown <markus99ml@gmail.com>
2017-05-24 20:05:29 +0200

    Bisschen was geändert wave spawning system ist noch nicht fertig bitte nichts dran ändern (also am Gamecontroller)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -51,20 +51,39 @@ public class GameController : MonoBehaviour
         }
     }
 
+    //zustaendig fuer das spawnen der Gegnerwellen
+
+    //UNFERTIG
     public void wavecontroll()
     {
-        if ((waveover == true) && (wave == 1))
+        if (waveover == true)
         {
-            GameObject newenemy = Instantiate(enemy) as GameObject;
-            newenemy.transform.position = new Vector3();
+            
         } 
     }
 
-    public void randompos()
+    //berechnet zufaellige x oder y koordinate fuer Gegnerspawnposition
+    public float randompos()
     {
-        float randx1 = Random.Range(-8.0f, (player.transform.position.x - 0.5f));
-        float randx2 = Random.Range((player.transform.position.x + 0.5f), 8.0f);
-        float randy1 = Random.Range(-8.0f, (player.transform.position.y - 0.5f));
-        float randy2 = Random.Range((player.transform.position.y + 0.5f), 8.0f);
+        float randx1 = Random.Range(-8.0f, (player.transform.position.x - 0.5f)); //zufaellige x-Koordinate Links oder unten vom Player
+        float randx2 = Random.Range((player.transform.position.x + 0.5f), 8.0f); //zufaellige x-Koordinate rechts oder oben vom Player
+        
+        int randud = Random.Range(1, 2); //Zufallszahl 1 oder 2 -> zum auswaehlen ob randx1 oder randx2
+
+        if (randud == 1)
+        {
+            return randx1;
+        }
+        else
+        {
+            return randx2;
+        }
+    }
+
+    public void wave1()
+    {
+        GameObject newenemy = Instantiate(enemy) as GameObject;
+        newenemy.transform.position = new Vector3(this.randompos(), this.randompos(), 0);
+        waveover = false;
     }
 }
