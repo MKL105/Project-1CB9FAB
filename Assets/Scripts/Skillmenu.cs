ff964e9a9901cb2c53eded34ed18eae2de24cb29
Unknown <markus99ml@gmail.com>
2017-06-07 14:50:44 +0200

    Pausen/Skillmenü
    Menü lässt sich öffnen hat aber noch keine funktionen

---
+++ Assets/Scripts/Skillmenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skillmenu : MonoBehaviour {
+
+    private bool ispaused;
+    public GameObject skillmenu;
+
+	void Start ()
+    {
+        ispaused = false;
+        skillmenu.gameObject.SetActive(false);
+	}
+	
+	void Update ()
+    {
+        if ((Input.GetKeyDown(KeyCode.P)))
+        {
+            ispaused = !ispaused;
+            if (ispaused)
+            {
+                pauseon();
+            }
+            else
+            {
+                pauseoff();
+            }
+        }
+	}
+
+    private void pauseon()
+    {
+        
+        Time.timeScale = 0; //pausiert des Spiel
+        skillmenu.gameObject.SetActive(true);
+    }
+
+    private void pauseoff()
+    {
+        Time.timeScale = 1; //setzt Spiel fort
+        skillmenu.gameObject.SetActive(false);
+    }
+}
