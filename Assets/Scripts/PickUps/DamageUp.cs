20a697a4c08d62d90dc6ee2cf94e7aa8f0092fdb
Unknown <markus99ml@gmail.com>
2017-06-28 08:42:29 +0200

    Everyone loves Gitkraken <3

---
+++ Assets/Scripts/PickUps/DamageUp.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageUp : MonoBehaviour
+{
+
+    private GameController gamecon;
+    public GameObject area;
+    public GameObject Player;
+    public PlayerController playercon;
+    public float DauerDamageUp;
+    private bool inuse = false;
+    private SpriteRenderer Sr;
+    private float aktuellerschaden;
+    void Start()
+    {
+        gamecon = area.GetComponent<GameController>();
+        playercon = Player.GetComponent<PlayerController>();
+        DauerDamageUp = 10f;
+        Sr = GetComponent<SpriteRenderer>();
+    }
+
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            aktuellerschaden = playercon.damage;
+            Color uns = Sr.color;
+            uns.a = 0;
+            Sr.color = uns;
+            if (inuse == false)
+            {
+
+                StartCoroutine(memes());
+
+            }
+        }
+    }
+
+    IEnumerator memes()
+    {
+        inuse = true;
+        playercon.damage = playercon.damage + 3.5f;
+
+        yield return new WaitForSeconds(DauerDamageUp);
+        playercon.damage = aktuellerschaden;
+        Destroy(gameObject);
+        inuse = false;
+    }
+}
---
+++ Assets/Scripts/PickUps/Instakill.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instakill : MonoBehaviour
+{
+
+    private GameController gamecon;
+    public GameObject area;
+    public GameObject Player;
+    public PlayerController playercon;
+    public float DauerInstakill;
+    private bool inuse = false;
+    private SpriteRenderer Sr;
+    private float aktuellerschaden;
+    void Start()
+    {
+        gamecon = area.GetComponent<GameController>();
+        playercon = Player.GetComponent<PlayerController>();
+        DauerInstakill = 10f;
+        Sr = GetComponent<SpriteRenderer>();
+    }
+
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            aktuellerschaden = playercon.erhoehterdamage;
+            Color uns = Sr.color;
+            uns.a = 0;
+            Sr.color = uns;
+            if (inuse == false)
+            {
+
+                StartCoroutine(inst());
+
+            }
+        }
+    }
+
+    IEnumerator inst()
+    {
+        inuse = true;
+        playercon.erhoehterdamage = playercon.erhoehterdamage + 9999999999999999f;
+
+        yield return new WaitForSeconds(DauerInstakill);
+        playercon.erhoehterdamage = aktuellerschaden;
+        Destroy(gameObject);
+        inuse = false;
+    }
+}
